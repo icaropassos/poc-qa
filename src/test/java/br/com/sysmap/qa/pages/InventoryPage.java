9d88201a7d4f625d8c2a13ff39bc82c9fe9fdc01
icaropassos <icaro.passos@sysmap.com.br>
2025-10-21 13:52:48 -0300

    Criar arquivo de page object para InventoryPage

---
+++ src/test/java/br/com/sysmap/qa/pages/InventoryPage.java
@@ -0,0 +1,8 @@
+package br.com.sysmap.qa.pages;
+
+import br.com.sysmap.qa.selenium.BasePage;
+
+public class InventoryPage extends BasePage {
+
+    // TODO: implementar métodos necessários para validações na página de inventário
+}
